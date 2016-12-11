188c34bd8dacbdfdb8641c188de51a7802b15035
Nick Behrens <behrensn@iastate.edu>
2016-12-10 19:58:10 -0600

    Adding turn indicator

--- app.js
+++ app.js
@@ -51,7 +51,6 @@ io.on('connection', function(socket) {
             users: {white: socket.userName, black: opponentid}
         };
 
-        socket.gameid = game.id;
         serverGames.push(game);
         console.log('Server Games is: '+serverGames);
         console.log('I just Added: '+game + ' to Server Games at: '+game.id);
@@ -63,6 +62,9 @@ io.on('connection', function(socket) {
         console.log("Starting Game "+game.id);
         activeUsers[game.users.white].emit('joinGame',{game:game,color:'white'});
         activeUsers[game.users.black].emit('joinGame',{game:game, color:'black'});
+
+        activeUsers[game.users.white].gameid = game.id;
+        activeUsers[game.users.black].gameid = game.id;
 
         playingUsers[game.users.white] = activeUsers[game.users.white];
         playingUsers[game.users.black] = activeUsers[game.users.black];
@@ -120,16 +122,74 @@ io.on('connection', function(socket) {
             socket.broadcast.emit('updateUsersAndGames',{users: Object.keys(activeUsers), games: serverGames});
         }
     });
+    socket.on('checkMate',function(data){
+        var game = data.game;
+        var username = data.username;
+        var end = data.end;
+
+        socket.broadcast.emit('endGame',{id:game.id});
+
+        if(data.color=="white"){
+            playingUsers[game.users.white].broadcast.emit('addUser',{id: playingUsers[game.users.white].userName});
+            activeUsers[game.users.white] = playingUsers[game.users.white];
+            delete playingUsers[game.users.white];
+        }else{
+            playingUsers[game.users.black].broadcast.emit('addUser',{id: playingUsers[game.users.black].userName});
+            activeUsers[game.users.black] = playingUsers[game.users.black];
+            delete playingUsers[game.users.black];
+        }
+/*
+        if(data.color=="white"){
+            playingUsers[game.users.white].emit('',{users: Object.keys(activeUsers)});
+        }else{
+            playingUsers[game.users.white].emit('',{users: Object.keys(activeUsers)});
+        }*/
+        
+        delete activeGames[game.id];
+        var i=0;
+        for(i-0;i<serverGames.length;i++){
+            if(serverGames[i].id==game.id){
+                serverGames.splice(i,1);
+            }
+        }
+
+        if(data.color=="white"){
+            activeUsers[game.users.white].emit('winnerCheckMate');
+            if(activeUsers[game.users.black]){
+                activeUsers[game.users.black].emit('loserCheckMate',{color:"black",game:game});
+            }else{
+                playingUsers[game.users.black].emit('loserCheckMate',{color:"black",game:game});
+            }
+            socket.broadcast.emit('updateUsersAndGames',{users: Object.keys(activeUsers), games: serverGames});
+        }else{
+            activeUsers[game.users.black].emit('winnerCheckMate');
+            if(activeUsers[game.users.white]){
+                activeUsers[game.users.white].emit('loserCheckMate',{color:"white",game:game});
+            }else{
+                playingUsers[game.users.white].emit('loserCheckMate',{color:"white",game:game});
+            }
+            socket.broadcast.emit('updateUsersAndGames',{users: Object.keys(activeUsers), games: serverGames});
+        }
+    });
     socket.on('backToLobby',function(data){
         var color = data.color;
         var game = data.game;
         console.log(color);
         if(color=="black"){
-            activeUsers[game.users.black] = playingUsers[game.users.black];
-            delete playingUsers[game.users.black];
-            activeUsers[game.users.black].emit('addUser',{id: activeUsers[game.users.black].userName});
-            activeUsers[game.users.black].emit('sendUserBackToLobby',{users: Object.keys(activeUsers)});
+            if(activeUsers[game.users.black]){
+                console.log('Active User is black.');
+                activeUsers[game.users.black].emit('addUser',{id: activeUsers[game.users.black].userName});
+                console.log('Added User');
+                activeUsers[game.users.black].emit('sendUserBackToLobby',{users: Object.keys(activeUsers)});
+            }else{
+                console.log('Playing user black: '+playingUsers[game.users.black].userName);
+                activeUsers[game.users.black] = playingUsers[game.users.black];
+                delete playingUsers[game.users.black];
+                activeUsers[game.users.black].emit('addUser',{id: activeUsers[game.users.black].userName});
+                activeUsers[game.users.black].emit('sendUserBackToLobby',{users: Object.keys(activeUsers)});
+            }
         }else{
+            console.log('Playing user white: '+playingUsers[game.users.white].userName);
             activeUsers[game.users.white] = playingUsers[game.users.white];
             delete playingUsers[game.users.white];
             activeUsers[game.users.white].emit('addUser',{id: activeUsers[game.users.white].userName});
@@ -145,6 +205,31 @@ io.on('connection', function(socket) {
 
         gameStates[gameId] = game;
         console.log('Added fen to game: '+gameId+", fen was: "+game);
+    });
+    socket.on('updateTurn',function(data){
+        console.log('Recieved Update Turn');
+        var game = data.game;
+        var color = data.color;
+
+        if(color=="white"){
+            if(playingUsers[game.users.black]){
+                console.log('User is playing');
+                console.log('Game id from call: '+game.id);
+                console.log('Game id from playing user '+ playingUsers[game.users.black].gameid);
+               if(playingUsers[game.users.black].gameid==game.id){
+                    console.log('User is playing game that is requesting turn');
+                    playingUsers[game.users.black].emit('ChangeTurns',{game:game});
+               } 
+            }
+        }else{
+            if(playingUsers[game.users.white]){
+                console.log('user is playing');
+               if(playingUsers[game.users.white].gameid==game.id){
+                    console.log('user is playing game that requested turn');
+                    playingUsers[game.users.white].emit('ChangeTurns',{game:game});
+               } 
+            }
+        }
     });
     socket.on('gameInitFen',function(data){
         gameStates[data.id] = data.gameFen;
@@ -163,19 +248,33 @@ io.on('connection', function(socket) {
 
         socket.broadcast.emit('leavelobby', socket.userName);
 
-        socket.gameid = game.id;
         activeGames[game.id] = game.id;
+
+        if(activeUsers[game.users.white]){
+            activeUsers[game.users.white].gameid = game.id;
+        }
+        if(activeUsers[game.users.black]){
+            activeUsers[game.users.black].gameid = game.id;
+        }
 
         console.log("White: "+game.users.white+", Black: "+game.users.black);
 
+        console.log("Socket Username: "+socket.userName);
+
         if(game.users.white == socket.userName){
+            console.log("User is white.");
             activeUsers[game.users.white].emit('joinGameInProgress',{game:game,gameState:gameState,color:'white'});
+            console.log("Active User: "+activeUsers[game.users.white]);
             playingUsers[game.users.white] = activeUsers[game.users.white];
             delete activeUsers[game.users.white];
+            console.log('Playing User: '+playingUsers[game.users.white]);
         }else{
+            console.log("User is black.");
             activeUsers[game.users.black].emit('joinGameInProgress',{game:game,gameState:gameState,color:'black'});
+            console.log("Active User: "+activeUsers[game.users.black]);
             playingUsers[game.users.black] = activeUsers[game.users.black];
             delete activeUsers[game.users.black];
+            console.log('Playing User: '+playingUsers[game.users.black]);
         }
     });
 });
